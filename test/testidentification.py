999fa47678458756b137d29675545c2f741dc315
remotephone <remotephone@gmail.com>
2019-10-12 17:09:40 -0500

    cleanup

--- test/testidentification.py
+++ test/testidentification.py
@@ -6,7 +6,6 @@ class TestResolve(unittest.TestCase):
     def test_is_shortened(self):
         services = define_services()
         for service in services:
-            print(service)
             assert is_shortened(
                 "http://{}/asdf".format(service)
             ), "http://{}/asdf".format(service)
